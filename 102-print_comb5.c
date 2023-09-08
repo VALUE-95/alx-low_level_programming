56f6fad8c58318e9ab6870442c945017d95858f1
OCHUBA Victor Ogechukwu <vochuba95@gmail.com>
2023-09-08 21:34:47 +0100

    two-digit numbers

---
+++ 102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two-digit numbers
+ *Then Return 0 Always (success)
+ */
+int main(void)
+{
+  int i, j;
+  for (i = 0; i < 100; i++)
+  {
+    for (j = 0; j < 100; j++)
+    {
+      if (i < j)
+        putchar((1 / 10) + 48);
+        putchar((i % 10) + 48);
+        putchar(' ')
+        putchar((j / 10) + 48);
+        putchar((j % 10) + 48);
+        if (i != 98 || j != 99)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+  }
+}
+putchar('\n');
+return (0)
+}
