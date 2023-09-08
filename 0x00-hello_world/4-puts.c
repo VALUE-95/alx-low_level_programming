1bbbefb59ccab0232e659e558bbc1fd8bdfb3b97
value <vochuba95@gmail.com>
2023-09-07 18:15:46 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Utilized the puts function
+ * Return: Always 0; (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
