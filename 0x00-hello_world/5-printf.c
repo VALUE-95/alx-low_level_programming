089ecb344affafbcc8441f8ac15faa153f9ca190
value <vochuba95@gmail.com>
2023-09-07 18:25:25 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** main - printed a programme on a newline using the function printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0)
+}
