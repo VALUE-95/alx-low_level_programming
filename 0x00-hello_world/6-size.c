590f070efa8b9b24b4db07a6b9063895f3314f86
value <vochuba95@gmail.com>
2023-09-11 16:45:04 -0700

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,3 +18,4 @@ printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
+}
