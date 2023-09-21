76a2fd0aa306317a1f3ccb3ac15483cef21b50fd
value <vochuba95@gmail.com>
2023-09-21 10:42:21 -0700

    More pointers, arrays and functions

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int q;
+
+	for (q = 0 ; q < n && src[q] != '\n' ; q++)
+		dest[q] = src[q];
+
+	while (q < n)
+	{
+		dest[q] = '\0';
+		q++;
+	}
+	return (dest);
+
+}
