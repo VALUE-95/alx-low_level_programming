ae789a4ec126a107571c1188be05c5ac51d49c8a
value <vochuba95@gmail.com>
2023-10-02 10:57:17 -0700

    Arguement count and vectors in C

---
+++ 0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - function to print programmme name
+ *@argc - argument count
+ *@argv - argument value
+ *return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s \n", argv[0]);
+	return (0);
+}
+
