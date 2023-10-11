08e7f9d64d363a6255188a19207030e8b09b06da
value <vochuba95@gmail.com>
2023-10-11 16:40:24 -0700

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: a pointer to the function
+ * print_name - prints a name.
+ * @name: the name.
+ * @f: pointer to function.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
 	f(name);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,8 +1,61 @@
-#include <stdio.h>
-#include "function_pointers.h"
+#include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+/**
+ * op_add - calculates the + of two integers
+ * @a: int 1.
+ * @b: int 2.
+ * Return: sum.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculates the + of two integers
+ * @a: int 1.
+ * @b: int 2.
+ * Return: difference.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates the * of two integers
+ * @a: int 1.
+ * @b: int 2.
+ * Return: product.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculates the / of two integers
+ * @a: int 1.
+ * @b: int 2.
+ * Return: result of the division.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the % of the division of two integers.
+ * @a: int 1.
+ * @b: int 2.
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
