a5d80e90685a0165f23247c82aca404e490457cc
value <vochuba95@gmail.com>
2023-10-19 13:39:25 -0700

    single

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t n;
+	size_t n = 0;
 
-	n = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[%d] %s\n", 0, "nil");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		n++;
 	}
