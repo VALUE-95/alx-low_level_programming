c539109fdf6da84508e65a983fc2ae627f77fd24
value <vochuba95@gmail.com>
2023-12-11 07:40:48 -0800

    try again

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o libhack101.so -fPIC hack101.c
-export LD_PRELOAD=./libhack101.so:$LD_LIBRARY_PATH
+gcc -shared -o myprintf.so -fPIC test.c
+export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH
