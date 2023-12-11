9de52cfba92c4c04bd06214819d34c85349465e4
value <vochuba95@gmail.com>
2023-12-11 07:48:40 -0800

    let us try again

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o myprintf.so -fPIC test.c
-export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH
+cp gm gm_backup && echo -e '1\n2\n3\n4\n5\n6' > gm
+sleep 98 && rm 101-make_me_win.sh
